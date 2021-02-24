097175896a6c30a41128ff406be19233026e63cd
Rylie Sweem <ryliesweem@gmail.com>
2021-02-24 10:23:55 -0800

    more styles

--- src/App.js
+++ src/App.js
@@ -6,20 +6,24 @@ import LinearProgress from '@material-ui/core/LinearProgress';
 
 function App() {
   const [text, setText] = useState('')
+  const[term, setTerm] = useState('')
   const [loading, setLoading] = useState(false)
   const [gifs, setGifs] = useState([])
 
   async function getGifs(){
+    setTerm('')
     setLoading(true)
-
     let url = 'https://api.giphy.com/v1/gifs/search?'
     url += 'api_key=' + 'jhQazp87aPuMIRIZoFu2kaI2Uk5GjZRJ'
     url += '&q=' + text
     const r = await fetch(url)
     const j = await r.json()
-    setGifs(j.data)
-    setLoading(false)
-    setText('')
+    if(j.data) {
+      setGifs(j.data)
+      setLoading(false)
+      setTerm(text)
+      setText('')
+    }
   }
 
   return (
@@ -34,13 +38,28 @@ function App() {
           Search
         </Button>
       </Header>
+
       {loading && <LinearProgress />}
-      <Body>
-        {gifs.map(m=> <Meme src={m.images.fixed_width.url}/>)}
-      </Body>
+
+      {gifs && gifs.length===0 && <Empty>
+        No gifs found! Try another search.
+      </Empty>}
+      
+      {gifs && gifs.length>0 && <Body>
+        {term && <Term>Showing results for: {term}</Term>}
+        {gifs.map(m=> <Gif src={m.images.fixed_width.url}/>)}
+      </Body>}
     </Wrap>
   );
 }
+
+const Term = styled.p`
+  font-weight: bold;
+  width: 100%;
+`
+const Empty = styled.p`
+  padding: 20px;
+`
 const Wrap = styled.div`
   display:flex;
   flex-direction:column;
@@ -56,6 +75,7 @@ const Header = styled.header`
 const Body = styled.div`
   width:100%;
   display:flex;
+  justify-content:space-between;
   flex-wrap:wrap;
   flex:1;
   overflow:auto;
@@ -63,12 +83,17 @@ const Body = styled.div`
   box-sizing: border-box;
   background-color: #f2f2f2;
 `
-const Meme = styled.img`
+const Gif = styled.img`
   max-height: 200px;
   max-width: 200px;
   min-width: 200px;
   object-fit: cover;
- padding: 10px;
+  padding: 10px;
+  @media only screen and (max-width: 480px) { 
+    max-height: 95%;
+    max-width: 95%;
+    min-width: 95%;
+  }
 `
 
 export default App;
